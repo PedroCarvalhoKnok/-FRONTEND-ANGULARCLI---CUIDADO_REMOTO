ed98ff8f278590256333cc81213186c230393e4d
Pedro Knok <fh141@t-systems.com.br>
2021-11-17 02:50:37 -0300

    FEAT - Função para geração de batimentos

--- CuidadoRemotoApp/src/app/heart-monitor/heart-monitor.component.ts
+++ CuidadoRemotoApp/src/app/heart-monitor/heart-monitor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-heart-monitor',
   templateUrl: './heart-monitor.component.html',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeartMonitorComponent implements OnInit {
 
+  listHeartBeatAtual!: any[];
+
+  //intervalId = setInterval(this.generateBeats(), 10000);
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  generateBeats(): void{
+
+    if(this.listHeartBeatAtual == undefined)
+        this.listHeartBeatAtual = [];
+        
+      const max = 80;
+      const min = 40;
+      let randomFrequency = Math.floor(Math.random() * (80 - 40 + 1) + 40);
+      this.listHeartBeatAtual.push(randomFrequency);
+      console.log(this.listHeartBeatAtual);
   }
+
+
+  ngOnInit(): void {
+    setInterval(this.generateBeats, 10000);
+  }
+  
 
 }
